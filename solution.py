85d83c3f5cef97622746d33e219da1b98a57e489
Christian Mondorf <christian@mondorf.dk>
2017-11-07 16:55:35 +0800

    Add boxes, units, and unitlist, all taken from courseware example.

--- solution.py
+++ solution.py
@@ -31,7 +31,18 @@ def cross(A, B):
     "Cross product of elements in A and elements in B."
     return [s+t for s in A for t in B] # source: courseware lesson 5.4
 
+rows = 'ABCDEFGHI'
+cols = '123456789'
 
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+
+column_units = [cross(rows, c) for c in cols]
+
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+unitlist = row_units + column_units + square_units
 
 def grid_values(grid):
     """
@@ -47,7 +58,7 @@ def grid_values(grid):
     i = 0
     for i in range(len(grid)):
        assign_value(sudoku_dict, boxes[i], grid[i])
-    return sudoku_dict 
+    return sudoku_dict
 
 def display(values):
     """
